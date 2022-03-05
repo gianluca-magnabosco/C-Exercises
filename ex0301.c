6ee6bb4addad84d89a23e259fd8b35d2aaee1094
gianluca-magnabosco <gianluca_1406@hotmail.com>
2022-03-04 23:42:14 -0300

    testing ex0301

--- ex0301.c
+++ ex0301.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int contador = 0, i = 0, num_aluno = 0;
-char questoes[7], gabarito[7];
+int contador = 0, i = 0, j = 0, nota = 0, num_aluno[9], media_aluno[9];
+char questoes[9][7], gabarito[7];
 
 void main(){
 
@@ -10,17 +10,31 @@ void main(){
     printf("Digite o gabarito da prova: ");
     for (i = 0; i < 8; i++){
         printf("Questão %d: ", i+1);
-        scanf("%d", gabarito[i]);
+        scanf("%c", gabarito[i]);
     }
 
+    for (j = 0; j < 10; j++){
+        printf("Digite o numero do aluno: ");
+        scanf("%d", num_aluno[j]);
+    
+        printf("Digite o gabarito do aluno %d: ", num_aluno[j]);
+        for (i = 0; i < 8; i++){
+            printf("\nQuestão %d: ", i+1);
+            scanf("%c", questoes[j][i]);
+            if (questoes[j][i] == gabarito[i]){
+                nota++;
+                
+                if (questoes[j][2] || questoes[j][7]){
+                    nota++;
+                }
+            }
+        
+        media_aluno[j] = nota;
+        nota = 0;
+        }
+    }
 
-    printf("Digite o numero do aluno: ");
-    scanf("%d", num_aluno);
-    
-    
-    printf("Digite o gabarito do aluno %d: ", num_aluno);
-    for (i = 0; i < 8; i++){
-        printf("\nQuestão %d: ", i+1);
-        scanf("%s", questoes[i]);
+    for (i = 0; i < 9; i++){
+        printf("Media do aluno %d: %d", num_aluno[i], media_aluno[i]);
     }
 }
