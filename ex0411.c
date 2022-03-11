e7e0505d84adc26e89bbb9f15528515f24156bbd
gianluca-magnabosco <gianluca_1406@hotmail.com>
2022-03-11 20:34:24 -0300

    done

---
+++ ex0411.c
@@ -0,0 +1,18 @@
+/*
+Seja vet um vetor de 4 elementos: TIPO vet[4]. 
+Supor que depois da declaração, vet esteja armazenado no endereço de memória 4092 (ou seja, o endereço de vet[0]).
+Supor também que na máquina usada, uma variável do tipo char ocupa 1 byte, do tipo int ocupa 2 bytes, 
+do tipo float ocupa 4 bytes e do tipo double ocupa 8 bytes.
+
+Qual o valor de vet+1, vet+2 e vet+3 se:
+
+a) vet for declarado como char? // 4093, 4094, 4095;
+
+b) vet for declarado como int? // 4094, 4096, 4098;
+
+c) vet for declarado como float? // 4096, 4100, 4104;
+
+d) vet for declarado como double? // 4100, 4108, 4116;
+
+
+*/
